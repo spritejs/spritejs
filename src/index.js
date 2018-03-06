60e7b2b2fbf07d4d85cf90bcd207229b5416176a
akira-cn <akira.cn@gmail.com>
2018-03-06 14:18:06 +0800

    :art:move server renderer to sprite-node

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
-import * as spritejs from './entrance'
+import BaseSprite from './basesprite'
+import Sprite from './sprite'
+import Label from './label'
+import Scene from './scene'
+import Resource from './resource'
+import Path from './path'
+import {parseColor} from './utils'
+import Axis from './axis'
+import {registerNodeType, createNode} from './nodetype'
+import Matrix from './matrix'
+import Group from './group'
+import {Effects} from 'sprite-animator'
 
-spritejs.serverRenderer = function () {
-  const {document, requestAnimationFrame} = require('./node-context')
-  global.document = document
-  global.requestAnimationFrame = requestAnimationFrame
-  global.IS_NODE_ENV = true
-  const {Sprite, Resource, Label, Color, Matrix, Group, Scene, Paper2D} = this
+const Color = parseColor
 
-  return {
-    Sprite,
-    Resource,
-    Label,
-    Color,
-    Matrix,
-    Group,
-    Scene,
-    Paper2D,
-  }
+function Paper2D(...args) {
+  console.warn('[Deprecation] spritejs.Paper2D is deprecated, instead use new spritejs.Scene.')
+  return new Scene(...args)
 }
 
-module.exports = spritejs
+export {
+  BaseSprite,
+  Sprite,
+  Resource,
+  Label,
+  Path,
+  Color,
+  Axis,
+  registerNodeType,
+  createNode,
+  Effects,
+  Matrix,
+  Group,
+  Scene,
+  Paper2D,
+}
