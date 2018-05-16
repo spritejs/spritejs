40fb6db067bd440ad12df725ac9b13aab0f6d962
akira-cn <akira.cn@gmail.com>
2018-05-16 21:10:48 +0800

    update

--- docs/src/coverpage.js
+++ docs/src/coverpage.js
@@ -341,14 +341,14 @@
     })
     fglayer.append(more)
 
-    more.animate([
-      {scale: 1},
-      {scale: 1.2},
-    ], {
-      duration: 1000,
-      iterations: Infinity,
-      direction: 'alternate',
-    })
+    // more.animate([
+    //   {scale: 1},
+    //   {scale: 1.2},
+    // ], {
+    //   duration: 1000,
+    //   iterations: Infinity,
+    //   direction: 'alternate',
+    // })
 
     document.querySelector('main').style.display = 'block'
     return more
