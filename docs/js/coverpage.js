b9f337f7f55c7b643856fad9e9413219a4f1108f
akira-cn <akira.cn@gmail.com>
2018-05-16 17:39:12 +0800

    update doc

--- docs/js/coverpage.js
+++ docs/js/coverpage.js
@@ -593,19 +593,19 @@ _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee6() {
             if (yOffset < 0) return;
 
             // console.log(yOffset)
-            if (!scrolled && yOffset) {
-              scrolled = true;
-              hideSprites([text].concat(_toConsumableArray(buttons), [more]));
-              guanguan.attr({
-                textures: ['guanguan3.png']
-              });
-            } else if (scrolled && yOffset === 0) {
-              scrolled = false;
-              showSprites([text].concat(_toConsumableArray(buttons), [more]));
-              guanguan.attr({
-                textures: ['guanguan1.png']
-              });
-            }
+            // if(!scrolled && yOffset) {
+            //   scrolled = true
+            //   hideSprites([text, ...buttons, more])
+            //   guanguan.attr({
+            //     textures: ['guanguan3.png'],
+            //   })
+            // } else if(scrolled && yOffset === 0) {
+            //   scrolled = false
+            //   showSprites([text, ...buttons, more])
+            //   guanguan.attr({
+            //     textures: ['guanguan1.png'],
+            //   })
+            // }
 
             if (yOffset >= maxScroll && coverpage.style.position !== 'absolute') {
               // coverpage.style.position = 'absolute'
