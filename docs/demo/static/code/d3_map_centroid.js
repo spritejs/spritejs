96fda02a32688c6085e630b12a3d98fb71a99807
Shero0311 <xzliang0311@163.com>
2018-06-19 17:23:49 +0800

    add demo-cursejs_simple

--- docs/demo/static/code/d3_map_centroid.js
+++ docs/demo/static/code/d3_map_centroid.js
@@ -33,7 +33,7 @@ window.helpers.loadScript(topojsonUrl).then(() => {
       .enter()
       .append('path')
       .attr('d', path)
-      .attr('strokeColor', '#EDF8DA')
+      .attr('strokeColor', '#618F4A')
       .attr('fillColor', '#70A556')
       .on('click', (d) => {
         const paths = d3.event.targetPaths
