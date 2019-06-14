190004c6c37e19ad31853d54a8157cf31895393f
akira-cn <akira.cn@gmail.com>
2019-06-14 18:01:04 +0800

    :memo:update docs

---
+++ docs/demo/static/code/echart_basic.js
@@ -0,0 +1,34 @@
+const {Scene, EChart} = spritejs;
+
+const scene = new Scene('#paper', {viewport: ['auto', 'auto'], resolution: [1200, 1200]});
+
+const layer = scene.layer('fglayer');
+document.querySelector('#paper canvas').style.backgroundColor = '#fff';
+
+const chart = new EChart({
+  anchor: [0.5, 0.5],
+  size: [700, 700],
+  pos: [600, 600],
+  // bgcolor: 'red',
+});
+
+chart.setOption({
+  title: {
+    text: 'ECharts 示例',
+  },
+  tooltip: {},
+  legend: {
+    data: ['销量'],
+  },
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+  },
+  yAxis: {},
+  series: [{
+    name: '销量',
+    type: 'bar',
+    data: [5, 20, 36, 10, 10, 20],
+  }],
+});
+
+layer.append(chart);
